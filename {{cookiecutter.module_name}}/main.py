3e8693ed2409549fbf042ae6f34e03e1c9ffa987
Zhaofeng <lzfxxx@gmail.com>
2019-08-08 14:13:21 +0800

    cn version comment

--- {{cookiecutter.module_name}}/main.py
+++ {{cookiecutter.module_name}}/main.py
@@ -5,8 +5,9 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
 
+# 请不要随意修改类名
 class {{cookiecutter.module_name}}(object):
-
+    
     def __init__(self, conf={}):
         '''
 
@@ -17,31 +18,33 @@ class {{cookiecutter.module_name}}(object):
 
     def train(self, conf={}):
         '''
-        training/fitting code for your model
+        在此编写模型的训练代码
         :param conf:
         :return:
         '''
+        # 参数和返回值请参照下面的格式填写, 以便自动生成参数配置
         param1 = conf['param1']  # value_type: str # description: some description
         param2 = conf['param2']  # value_type: str # description: some description
-        # add your code
+        # 编写你的代码
         return {'ret1': 'cat', 'ret2': 'dog'}
 
     def predict(self, conf={}):
         '''
-        predict/inference code for your model
+        在此编写模型预测代码
         :param conf:
         :return:
         '''
+        # 参数和返回值请参照下面的格式填写, 以便自动生成参数配置
         param1 = conf['param1']  # value_type: str # description: some description
         param2 = conf['param2']  # value_type: str # description: some description
-        # add your code
+        # 编写你的代码
         return {'ret1': 'cat', 'ret2': 'dog'}
 
     def load_model(self):
         '''
-        load your model weight and structure
+        在此加载模型的结构以及权重， 并返回模型实例
         :param conf:
-        :return: model instance
+        :return: 模型实例
         '''
         pass
  
